433e466ddaa5e68398cbf5d2557bcd72906e400a
yuanyuan-su <107480786+yuanyuan-su@users.noreply.github.com>
2026-04-24 14:45:29 +0000

    Deploying to gh-pages from @ yuanyuan-su/yuanyuan-su.github.io@75a8583d68686f35d9af3fa642849c1963a25e73 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,7 +511,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/cv.pdf", "_blank");
+          window.open("/assets/pdf/CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
