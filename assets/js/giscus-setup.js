9f50df5ed615b764125534fd97ecf54674b925ee
yuanyuan-su <107480786+yuanyuan-su@users.noreply.github.com>
2026-04-24 09:49:01 +0000

    Deploying to gh-pages from @ yuanyuan-su/yuanyuan-su.github.io@63363ef093d15afa83f070b6757fb71c664fde60 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "CccccccCieL/CccccccCieL.github.io",
+    "data-repo": "yuanyuan-su/yuanyuan-su.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
